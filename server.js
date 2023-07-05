1bebbd5f7834f723a9d71c093ec95cf3f9ed3c41
Carmine Acanfora <carminea@jfrog.com>
2023-07-05 10:01:07 +0100

    Add source code

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+const lodash = require('lodash'); 
+const evilsrc = {constructor: {prototype: {evilkey: "evilvalue"}}};
+lodash.defaultsDeep({}, evilsrc)
+
+var app = express();
+
+var myLogin = "AKIAJXBOVX5Q2EULDUIA";
+var mypwd = "SqcyDpetv+pCsbNYWHDLE8yR5mJ13MI+4d8NOwtM";
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// static assets directory
+app.use(express.static('public'));
+
+// index page, this callback contains code that can be exploited for CVE-2022-29078 
+app.get('/', function(req, res) {
+  if (!req.query.hasOwnProperty('id')){
+    req.query.id = 'Stranger';
+  }
+  res.render('pages/index',req.query);
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
