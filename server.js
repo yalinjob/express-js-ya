06d1c083acffe99827861af711c2ef2f076aae76
Carmine Acanfora <carminea@jfrog.com>
2024-03-13 13:42:06 +0000

    Update dependencies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const lodash = require('lodash'); 
 const evilsrc = {constructor: {prototype: {evilkey: "evilvalue"}}};
+// Object.freeze(Object.prototype);
 lodash.defaultsDeep({}, evilsrc)
 
 var app = express();
@@ -24,3 +25,17 @@ app.get('/', function(req, res) {
 
 app.listen(3000);
 console.log('Server is listening on port 3000');
+
+var session = require("express-session")
+app.use(
+  session({
+    cookie: {
+      domain: "domain.org",
+      secure: true,
+      httpOnly: false,
+      name: "cookie",
+      maxAge: 12 * 60 * 60 * 1000,
+      path: "/",
+    },
+  })
+)
